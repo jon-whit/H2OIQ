5753e56aa3514c09595d3c773fca6939d8b4abcf
Jonathan Whitaker <jon.b.whitaker@gmail.com>
2015-04-24 21:34:11 -0600

    Added initial URL layout and working skeleton for stations view.

--- H2OIQ/urls.py
+++ H2OIQ/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'H2OIQ.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^stations/', include('stations.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
